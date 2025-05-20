f24b19294383ed334543416a25eebdb6ff94c6d8
Janet Tong <jtong50@stuy.edu>
2025-05-20 15:34:53 -0400

    write bytes to file

--- audio.py
+++ audio.py
@@ -2,7 +2,10 @@ import wave
 
 def audio_bits(path): 
     audio = wave.open(path, mode = 'rb')
-    arr = list(audio.readframes(audio.getnframes()))
-    print(type(arr))
+    arr = list(audio.readframes(audio.getnframes())) #converts frames into a list
+    arr = bytearray(arr) #converts to bytes
+    with open("temp.txt", "wb") as f:
+        f.write(arr)
+        
 
 audio_bits('cyber.wav')
