5ceffdb85342fdfdebc2bb2ed74db91882812129
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2025-05-20 23:15:48 -0400

    set up methods

---
+++ encode.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+
+public class encode{
+	
+	public static byte[] getBytes(String filename){
+		return new byte[1];
+		//return bytes extracted from file
+	}
+	
+	public static byte[] encode(byte[] bytes, String message){
+		return new byte[1];
+		//return modified array of bytes
+	}
+	
+	
+	public static void main(String[] args){
+		
+	}
+}
