e377e30dcf72211c41e65c4cc50a75c62b995149
Janet Tong <jtong50@stuy.edu>
2025-05-23 15:35:53 -0400

    started decode

---
+++ decode.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.io.*;
+import java.nio.*;
+
+public class decode{
+	
+	public static byte[] getBytes(String filename){ 		//return bytes extracted from file
+		byte[] bytes = new byte[1];
+		try{
+			File f = new File(filename);
+			FileInputStream file = new FileInputStream(filename);
+			bytes = new byte[(int)f.length()];
+			file.read(bytes);
+		}catch(IOException e){
+			System.out.println("File Not Found");
+		}
+	
+		//testing
+		/* for(int i = 0; i < bytes.length; i++){
+			System.out.println((Integer)Byte.toUnsignedInt(bytes[i])+" ");
+		} */
+	 // System.out.println(Arrays.toString(bytes));
+
+		return bytes;
+	}
+	
+	
+	//A = 01000001
+	
+	public static byte[] decode(byte[] bytes, String message){
+		byte[] msg = new byte[message.length()*4];
+		for(int i = 0; i < message.length(); i++){
+			byte character = (byte)message.charAt(i);
+			for(int j = 4; j > 0; j--){
+				msg[i*4 + j - 1] = (byte)((character >> (2*(j-1)))&3);
+			}
+		}
+	 // System.out.println(Arrays.toString(msg));
+		
+		for(int i = 0; i < msg.length; i++){
+			bytes[i] = (byte)(bytes[i] | msg[i]);
+		}
+	//	System.out.println(Arrays.toString(bytes));
+		return bytes;
+		//return modified array of bytes
+	}
+	
+	
+	public static void main(String[] args){
+		//encode(getBytes(args[0]), "A");
+		encode(getBytes(args[0]), "A");
+	}
+}
