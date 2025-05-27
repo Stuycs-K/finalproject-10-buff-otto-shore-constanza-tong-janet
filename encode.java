fe120da27ac87dd15eb0492fe84e68577d5b327a
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2025-05-27 15:22:32 -0400

    testing

--- encode.java
+++ encode.java
@@ -15,36 +15,42 @@ public class encode{
 			System.out.println("File Not Found");
 		}
 	
-	 // System.out.println(Arrays.toString(bytes));
-
+		/*for(int i = 0; i < 4*4; i++){
+			System.out.print((Integer)Byte.toUnsignedInt(bytes[i])+" ");
+		}*/
 		return bytes;
 	}
 	
 	
 	//A = 01000001
+	//B = 01000010
 	
 	public static byte[] encode(byte[] bytes, String message){
 		byte[] msg = new byte[message.length()*8];
 		for(int i = 0; i < message.length(); i++){
 			byte character = (byte)message.charAt(i);
 			for(int j = 8; j > 0; j--){
-				msg[i*8 + j - 1] = (byte)((character >> ((j-1)))&1);
-			}
-		}
+				//msg[i*8 + j - 1] = (byte)((character >> ((j-1)))&1);
+				msg[(i+1)*8 - j ] = (byte)((character >> ((j-1)))&1);
 				
+			}
+		}				
 		for(int i = 0; i < msg.length; i++){
 			bytes[i] = (byte)(bytes[i] | msg[i]);
 		}
 		for(int i = msg.length; i < bytes.length; i++){
 			bytes[i] = (byte)(bytes[i] | 0);
 		}
-	//	System.out.println(Arrays.toString(bytes));
+		
+		/*for(int i = 0; i < msg.length; i++){
+			System.out.print((Integer)Byte.toUnsignedInt(bytes[i])+" ");
+		}*/
 		return bytes;
 	}
 	
 	
 	public static void main(String[] args){
 		//getBytes(args[0]);
-		encode(getBytes(args[0]), "A");
+		encode(getBytes(args[0]), args[1]);
 	}
 }
