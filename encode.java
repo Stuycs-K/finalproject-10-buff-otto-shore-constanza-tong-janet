2cafb7254c77f3f8cf20553cc233c61dbc6dbc96
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2025-05-27 15:30:22 -0400

    encode

--- encode.java
+++ encode.java
@@ -30,24 +30,33 @@ public class encode{
 		for(int i = 0; i < message.length(); i++){
 			byte character = (byte)message.charAt(i);
 			for(int j = 8; j > 0; j--){
-				//msg[i*8 + j - 1] = (byte)((character >> ((j-1)))&1);
-				msg[(i+1)*8 - j ] = (byte)((character >> ((j-1)))&1);
+				msg[i*8 + j - 1] = (byte)((character >> ((j-1)))&1);
+				//msg[(i+1)*8 - j ] = (byte)((character >> ((j-1)))&1);
 				
 			}
 		}				
 		for(int i = 0; i < msg.length; i++){
 			bytes[i] = (byte)(bytes[i] | msg[i]);
 		}
+		System.out.println(Arrays.toString(msg));
+		
 		for(int i = msg.length; i < bytes.length; i++){
 			bytes[i] = (byte)(bytes[i] | 0);
 		}
 		
-		/*for(int i = 0; i < msg.length; i++){
+		for(int i = 0; i < msg.length; i++){
 			System.out.print((Integer)Byte.toUnsignedInt(bytes[i])+" ");
-		}*/
+		}
 		return bytes;
 	}
 	
+	/*public static byte[] encode2(byte[] bytes, String message){
+		byte[] msg = new byte[message.length()*8];
+		for(int i = 0; i < message.length(); i++){
+			byte character = (byte)message.charAt(i);
+			for(int j = 8; j > 0)
+		}
+	}*/
 	
 	public static void main(String[] args){
 		//getBytes(args[0]);
