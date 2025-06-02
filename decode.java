8c2e26ae2f19ee7b3739115178f0717a9e55b04c
ottobuff180 <obuff50@stuy.edu>
2025-06-02 12:00:39 -0400

    Changes to decode, rewrote most of it to work

--- decode.java
+++ decode.java
@@ -1,49 +1,35 @@
-public class Decode extends ReadFile{
-  public static void main(String[] args){
-    int[][] arr = new arr[][];
-    arr = readf("temp.txt");
-    String message = decodeMessage(arr);
-  }
-  
-  public static String decodeMessage(int[][] arr) {
-    StringBuilder newstr = new StringBuilder();
-    
-    if (arr != null) {
-      for (int[] row : arr) {
-        if (row != null) {
-          for (int i : row) {
-            if (i > 1 || i < 0){
-              System.out.println("Invalid binary, no more bits will be read");
-              break;
-            }
-            newstr.append(i);
-          }
-        }
-      }
-    }
-    
-    String str = newstr.toString();
-    StringBuilder message = new StringBuilder();
-    
-    for (int i = 0; i < str.length(); i += 8) {
-      
+import java.io.*;
 
-      if (i + 8 > str.length()) {
-        System.out.println((str.length() - i) + " incomplete bytes.");
-        break;
-      }
-      
-      String onebyte = str.substring(i, i + 8);
-      
-      int asc = Integer.parseInt(onebyte, 2);
-      
-      if (asc == 0) {
-        break;
-      }
-      
-      message.append((char) asc);
-    }
-    
-    return message.toString();
-  }
+public class Decode {
+
+    public static String decode(byte[] bytes) {
+        StringBuilder msg = new StringBuilder();
+        int p = 0;
+        byte l = 0;
+        int b = 0;
+        for (byte b : bytes) {
+            l |= ((b & 1) << b);
+            b++;
+            if (b == 8) {
+                if (l == 0xFF)
+                  break;
+                msg.append((char) (l & 0xFF));
+                l = 0;
+                b = 0;
+            }
+        }
+        return msg.toString();
+    }
+
+    public static void main(String[] args) {
+        
+        String bf = args[0];
+        String tf = args[1];
+        try (FileOutputStream out = new FileOutputStream(tf)) {
+            out.write(decode(getBytes(bf)).getBytes());
+        } catch (IOException e) {
+            System.err.println("Error, can't write to file: " + e.getMessage());
+            System.exit(1);
+        }
+    }
 }
