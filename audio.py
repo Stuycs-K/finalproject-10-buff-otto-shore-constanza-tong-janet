083022366b1d54972187e0385c70a05cf463cefe
Janet Tong <jtong50@stuy.edu>
2025-06-03 19:40:04 -0400

    encode + back in audio in python

--- audio.py
+++ audio.py
@@ -1,38 +1,52 @@
 import wave 
-import shutil 
+import shutil
 import matplotlib.pyplot as plt 
 import scipy.io.wavfile as wavfile
 
 import sys
 
-#path: the audio file name 
-def audio_to_bits(path): 
+#path: the audio file name
+#new_path: the new audio file name
+#converts the wav file into a byte array 
+def audio_to_bits(path, new_path): 
+    shutil.copy(path, new_path)
     audio = wave.open(path, mode = 'rb')
     arr = list(audio.readframes(audio.getnframes())) #converts frames into a list
     arr = bytearray(arr) #converts to bytes
-    try: 
-        with open("temp.txt", "wb") as f: #write to file
-            f.write(arr)
-    except FileNotFoundError: 
-        print("File not found")
 
+#message: the message to be encoded 
+#converts the message to a bit array
+def message_to_bits(message): 
+    bit_array = [] 
+    for char in message: 
+        binary = bin(ord(char))[2:] 
+        binary = '0' * (8 - len(binary)) + binary #get bit string for each char
+        for b in binary: 
+            bit_array.append(int(b))
+    return bit_array 
+        
+#message: message in bit array 
+#byte_array: the byte array of the audio
+#encodes each bit into the lsb of the byte array
+def encode(message, byte_array): 
+    message += int((len(byte_array) - len(message)*8*8)/8) * '&'
+    message_array = message_to_bits(message)
+    for i in range(len(byte_array)): 
+        byte_array[i] = (byte_array[i] & 254) | message_array[i]
+    return bytes(byte_array)
 
 #path: the modified audio file name
-def bits_to_audio(path, new_path): 
-    shutil.copy(path, new_path)
-    try: 
-        with open("encoded.txt", "rb") as f: #read bytes from file
-            byte_arr = bytearray(f.read())
-    except FileNotFoundError: 
-        print("File not found")
+#byte_array: the modified bytes
+def bits_to_audio(path, byte_array): 
+    with wave.open(path, 'wb') as wav: 
+        wav.writeframes(byte_array) 
 
-    with wave.open(new_path, 'wb') as wav: 
-        wav.writeframes(byte_arr)
+
 
 if len(sys.argv) == 2: 
     audio_to_bits('wavFiles/raw/' + sys.argv[1])
 else: 
-    bits_to_audio('wavFiles/raw/' +sys.argv[0], 'wavFiles/encoded/' + sys.argv[1])
+    bits_to_audio('wavFiles/raw/' +sys.argv[1], 'wavFiles/encoded/' + sys.argv[2])
 
 
 
