3a71f54ca28a020ee8d7807ca8f84d6ffaa333cd
Constanza Shore Coloni <cshorecoloni50@stuy.edu>
2025-06-03 21:30:44 -0400

    decode variations

--- decode.py
+++ decode.py
@@ -19,12 +19,34 @@ def decode(byte_array, increment):
     
     return message.split("&&&&")[0]
 
+def decode2(byte_array, increment): 
+    message_bits = ""
+    message = ""
+    for i in range(0, len(byte_array), increment): 
+        curr_byte = byte_array[i]
+        if (curr_byte & 192) == 192:
+            message_bits += str(byte_array[i] & 1)
+    for i in range(0, len(message_bits), 8): 
+        message += chr(int(message_bits[i:i+8], 2))
+    
+    return message.split("&&&&")[0]
+
+
+
 def main(path, increment): 
     byte_array = audio_to_bits(path) 
     message = decode(byte_array, increment) 
     print(message)
+    
+def main2(path, increment): 
+    byte_array = audio_to_bits(path) 
+    message = decode2(byte_array, increment) 
+    print(message)
 
 if (len(sys.argv) - 1) == 2:
-    main('wavFiles/encoded/' + sys.argv[1], sys.argv[2])
+    if sys.argv[2] == 'msb':
+        main2('wavFiles/encoded/' + sys.argv[1], sys.argv[2])
+    else:
+        main('wavFiles/encoded/' + sys.argv[1], sys.argv[2])
 else:
     main('wavFiles/encoded/' + sys.argv[1], 1)
